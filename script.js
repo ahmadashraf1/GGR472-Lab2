1a68f4172c8941f13de402761e7dd58bc159fdb4
Ahmad Ashraf <ahmad.ashraf@mail.utoronto.ca>
2024-02-10 09:00:49 -0500

    Updated Version

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWhtYWRhc2hyYWYxNTQiLCJhIjoiY2xyaTYzNXlsMDM0eDJpcnhtY3lnb2QzdCJ9.PUhrzYu0LU7a6_Up5_Q-eA'; //Add default public map token from your Mapbox account
 
+/// Creating map
 const map = new mapboxgl.Map({
-container: 'my-map', // map container ID
-style: 'mapbox://styles/ahmadashraf154/clsg2244503fc01qq3sngbyp4', // style URL
-center: [-79.3923,43.6643], // starting position [lng, lat]
-zoom: 14.3, // starting zoom
+container: 'my-map', /// map container ID
+style: 'mapbox://styles/ahmadashraf154/clsg2244503fc01qq3sngbyp4', /// style URL
+center: [-79.3923,43.6643], /// starting position [lng, lat]
+zoom: 14.3, /// starting zoom
 });
 
 map.on('load', () => {
 
-    //Add a data source containing GeoJSON data
+    /// Adding a data source containing GeoJSON data
+    /// UofT Buildings
     map.addSource('uoft-data', { 
         type: 'geojson',
         data: {
@@ -18,7 +20,7 @@ map.on('load', () => {
                 {
                     "type": "Feature", 
                     "properties": {
-                        "name": "Sidney Smith Hall" 
+                        "name": "Sidney Smith Hall"  /// Adding first point
                     },
                     "geometry": { 
                         "coordinates": [
@@ -31,7 +33,7 @@ map.on('load', () => {
                 {
                     "type": "Feature",
                     "properties": {
-                        "name": "Bahen Centre"
+                        "name": "Bahen Centre"  /// Adding second point
                     },
                     "geometry": {
                         "coordinates": [
@@ -44,7 +46,7 @@ map.on('load', () => {
                 {
                     "type": "Feature",
                     "properties": {
-                        "name": "Medical Sciences Building"
+                        "name": "Medical Sciences" /// Adding third point
                     },
                     "geometry": {
                         "coordinates": [
@@ -67,5 +69,47 @@ map.on('load', () => {
             'circle-color': '#B42222' 
         }
     });
+
+    /// Adding a data source from a GeoJSON file
+    /// UofT Sports Facilities
+    map.addSource('sports-data', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/ahmadashraf1/GGR472-Lab2/main/sports.geojson' /// URL to sports.geojson file
+    });
+
+    map.addLayer({
+        'id': 'sports-point',
+        'type': 'circle',
+        'source': 'sports-data',
+        'paint': {
+            'circle-radius': 6,
+            'circle-color': '#AB65F8' }
+    });
+
+    
+    /// Open a popup when a feature in UofT Buildings layer is clicked, with the name of the building.
+    map.on('click', 'uoft-pnt', (e) => {
+        const coordinates = e.features[0].geometry.coordinates.slice(); /// get coordinates
+        const name = e.features[0].properties.name; /// get building name
+
+        new mapboxgl.Popup() /// creating popup
+            .setLngLat(coordinates) /// setting coordinates
+            .setHTML('<p>' + "Building Name: " + name + '</p>') /// adding HTML
+            .addTo(map);
+    });
+
+    /// Open a popup when a feature in UofT Sports Facilities layer is clicked, with the name of the building.
+    map.on('click', 'sports-point', (e) => {
+        const coordinates = e.features[0].geometry.coordinates.slice(); /// get coordinates
+        const name = e.features[0].properties.buildingname; /// get building name
+
+        new mapboxgl.Popup() /// creating popup
+            .setLngLat(coordinates) /// setting coordinates
+            .setHTML('<p>' + "Building Name: " + name + '</p>') /// adding HTML
+            .addTo(map);
+    });
+
+    document.getElementById('state-legend'); /// adding legend and title
+    document.getElementById('title').innerHTML = "University of Toronto - St. George Map"
 
 });
